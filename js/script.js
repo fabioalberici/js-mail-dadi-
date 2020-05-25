8af166dfa09c1aceb5dc52df0c7682d543c419ef
fabioalberici <fabioalbe79@gmail.com>
2020-05-25 21:21:03 +0200

    consegna dadi (?) continuo a non riuscire a rappresentare

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ var email = ["abc@gmail.it", "abc@yahoo.it", "abc@msn.it", "abc@libero.it"];
 var abcGiusto = false;
 var emailValida  = 'abc';
 //richiesta una email
+
 var inserisci = prompt('Inserisci una mail valida');
 
-// principio di validità
+//principio di validità
 for ( var i = 0; 1 < email.length; i++)
   if (email[i] === emailValida ) {
     emailValida = true;
@@ -16,7 +17,17 @@ if (emailValida) {
   prompt('email non valida!')
 }
 
-
 //////////////////////////////////////////
 
-esercizio dadi 
+esercizio dadi
+
+var dadoUtente = parseInt(Math.floor(Math.random() * 6));
+var dadoCpu = parseInt(Math.floor(Math.random() * 6));
+
+if () {
+  dadoUtente > dadoCpu = prompt('hai vinto')
+} else if () {
+  dadoUtente > dadoCpu = prompt('hai perso')
+} else () {
+ dadoUtente == dadoCpu = prompt('hai pareggiato')
+}
