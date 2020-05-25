66e90155013e48c383cdc86b4e9bd4e2acb23838
fabioalberici <fabioalbe79@gmail.com>
2020-05-25 21:13:48 +0200

    prova esercizio mail, ma non riesco a far rispondere  a dover il browser

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var email = ["abc@gmail.it", "abc@yahoo.it", "abc@msn.it", "abc@libero.it"];
+var abcGiusto = false;
+var emailValida  = 'abc';
+//richiesta una email
+var inserisci = prompt('Inserisci una mail valida');
+
+// principio di validità
+for ( var i = 0; 1 < email.length; i++)
+  if (email[i] === emailValida ) {
+    emailValida = true;
+}
+
+if (emailValida) {
+  prompt('email valida!')
+} else if {
+  prompt('email non valida!')
+}
+
+
+//////////////////////////////////////////
+
+esercizio dadi 
